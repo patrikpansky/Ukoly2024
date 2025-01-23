91a52809508b5b25644fb76d0f55623b58632588
patrikpansky <patricek.pansky@seznam.cz>
2025-01-23 10:07:24 +0100

    source.cpp

--- Ukol_5/cpp/source.cpp
+++ Ukol_5/cpp/source.cpp
@@ -1,4 +1,14 @@
-int main()
-{
+#include "prostredi.h"
+#include <iostream>
+
+int main() {
+    Prostredi svet(20, 20);
+
+    svet.pridatOrganismus(new Rostlina(5, 5));
+    svet.pridatOrganismus(new Zivocich(10, 10));
+    svet.pridatOrganismus(new Zivocich(15, 15));
+
+    svet.simuluj(10);
+
     return 0;
 }
