9af9db4881e68ce71a26e5e11b213be05ec16fb8
patrikpansky <patricek.pansky@seznam.cz>
2025-01-27 13:26:31 +0100

    linked_list.cpp

--- Ukol_3/cpp/linked_list.cpp
+++ Ukol_3/cpp/linked_list.cpp
@@ -15,32 +15,32 @@ Node* createNode(int data) {
     return newNode;
 }
 
-void insertAtBeginning(Node** head, int data) {
+void insertAtBeginning(Node*& head, int data) {
     Node* newNode = createNode(data);
-    newNode->next = *head;
-    *head = newNode;
+    newNode->next = head;
+    head = newNode;
 }
 
-void insertAtEnd(Node** head, const int data) {
+void insertAtEnd(Node*& head, const int data) {
     Node* newNode = createNode(data);
-    if (*head == nullptr) {
-        *head = newNode;
+    if (head == nullptr) {
+        head = newNode;
         return;
     }
-    Node* temp = *head;
+    Node* temp = head;
     while (temp->next != nullptr) {
         temp = temp->next;
     }
     temp->next = newNode;
 }
 
-void insertAtIndex(Node** head, int data, int index) {
+void insertAtIndex(Node*& head, int data, int index) {
     if (index == 0) {
         insertAtBeginning(head, data);
         return;
     }
     Node* newNode = createNode(data);
-    Node* temp = *head;
+    Node* temp = head;
     for (int i = 0; temp != nullptr && i < index - 1; i++) {
         temp = temp->next;
     }
@@ -53,21 +53,21 @@ void insertAtIndex(Node** head, int data, int index) {
     temp->next = newNode;
 }
 
-void deleteAtBeginning(Node** head) {
-    if (*head == nullptr) return;
-    Node* temp = *head;
-    *head = (*head)->next;
+void deleteAtBeginning(Node*& head) {
+    if (head == nullptr) return;
+    Node* temp = head;
+    head = head->next;
     delete temp;
 }
 
-void deleteAtEnd(Node** head) {
-    if (*head == nullptr) return;
-    if ((*head)->next == nullptr) {
-        delete* head;
-        *head = nullptr;
+void deleteAtEnd(Node*& head) {
+    if (head == nullptr) return;
+    if (head->next == nullptr) {
+        delete head;
+        head = nullptr;
         return;
     }
-    Node* temp = *head;
+    Node* temp = head;
     while (temp->next->next != nullptr) {
         temp = temp->next;
     }
@@ -75,13 +75,13 @@ void deleteAtEnd(Node** head) {
     temp->next = nullptr;
 }
 
-void deleteAtIndex(Node** head, int index) {
-    if (*head == nullptr) return;
+void deleteAtIndex(Node*& head, int index) {
+    if (head == nullptr) return;
     if (index == 0) {
         deleteAtBeginning(head);
         return;
     }
-    Node* temp = *head;
+    Node* temp = head;
     for (int i = 0; temp != nullptr && i < index - 1; i++) {
         temp = temp->next;
     }
@@ -103,12 +103,12 @@ int findFirstOccurrence(Node* head, int value) {
         head = head->next;
         index++;
     }
-    return -1; 
+    return -1;
 }
 
-void sortList(Node** head) {
-    if (*head == nullptr) return;
-    for (Node* i = *head; i != nullptr; i = i->next) {
+void sortList(Node*& head) {
+    if (head == nullptr) return;
+    for (Node* i = head; i != nullptr; i = i->next) {
         for (Node* j = i->next; j != nullptr; j = j->next) {
             if (i->data > j->data) {
                 std::swap(i->data, j->data);
@@ -117,8 +117,8 @@ void sortList(Node** head) {
     }
 }
 
-void deleteList(Node** head) {
-    while (*head != nullptr) {
+void deleteList(Node*& head) {
+    while (head != nullptr) {
         deleteAtBeginning(head);
     }
 }
@@ -137,20 +137,20 @@ std::ostream& operator<<(std::ostream& os, Node* head) {
 #ifndef __TEST__
 int main() {
     Node* head = nullptr;
-    insertAtBeginning(&head, 1);
-    insertAtBeginning(&head, 2);
-    insertAtEnd(&head, 3);
+    insertAtBeginning(head, 1);
+    insertAtBeginning(head, 2);
+    insertAtEnd(head, 3);
     std::cout << "Seznam po vložení prvků: " << head << std::endl;
 
     std::cout << "První výskyt hodnoty 3 je na indexu: " << findFirstOccurrence(head, 3) << std::endl;
-    sortList(&head);
+    sortList(head);
     std::cout << "Seznam po třídění: " << head << std::endl;
 
-    deleteAtBeginning(&head);
-    deleteAtEnd(&head);
+    deleteAtBeginning(head);
+    deleteAtEnd(head);
     std::cout << "Seznam po smazání prvků: " << head << std::endl;
 
-    deleteList(&head);
+    deleteList(head);
 
     return 0;
 }
